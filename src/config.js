0e98485f4397130d90b6f7c1bcd667138def994e
xiang-921101 <132485665+xiang-921101@users.noreply.github.com>
2024-10-11 21:58:21 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -44,7 +44,5 @@ module.exports = {
     style: processStyle(core.getInput('OUTPUT_STYLE')),
     ignoreEvents: processIgnoreEvents(core.getInput('IGNORE_EVENTS')),
     readmePath: core.getInput('README_PATH'),
-    commitMessage: core.getInput('COMMIT_MESSAGE'),
-    targetRepos: ['repo1/owner1', 'repo2/owner2']  // Replace with your desired repositories
+    commitMessage: core.getInput('COMMIT_MESSAGE')
 };
-
