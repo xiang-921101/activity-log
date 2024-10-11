1f217cd6ae6d616828f7b9111eb2003249eec46a
xiang-921101 <132485665+xiang-921101@users.noreply.github.com>
2024-10-11 21:50:30 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const { fetchAndFilterEvents } = require('./utils/github');
 const { updateReadme } = require('./utils/file');
-const { username, token, eventLimit, ignoreEvents, readmePath, commitMessage } = require('./config');
+const { username, token, eventLimit, ignoreEvents, readmePath, commitMessage, targetRepos } = require('./config');
 const core = require('@actions/core')
 
 // Main function to execute the update process
 async function main() {
     try {
-        const activity = await fetchAndFilterEvents({ username, token, eventLimit, ignoreEvents });
+        // Add `targetRepos` to the fetchAndFilterEvents call
+        const activity = await fetchAndFilterEvents({ username, token, eventLimit, ignoreEvents, targetRepos });
         await updateReadme(activity, readmePath);
     } catch (error) {
         core.setFailed(`❌ Error in the update process: ${error.message}`);
