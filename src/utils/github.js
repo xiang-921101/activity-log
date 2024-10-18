1e073ef3902535cd5ed3fb3be8ddcc4d12e11aef
xiang-921101 <132485665+xiang-921101@users.noreply.github.com>
2024-10-18 11:26:24 +0800

    Update github.js

--- src/utils/github.js
+++ src/utils/github.js
@@ -73,7 +73,7 @@ async function fetchAllEvents() {
 
             // Check for API rate limit or pagination issues
             if (events.length === 0) {
-                core.warning('⚠️ No more events available.');
+                core.warning('⚠️T3： No more events available.');
                 break; // No more events to fetch
             }
 
